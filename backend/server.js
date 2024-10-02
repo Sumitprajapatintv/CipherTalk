f17a647d6a9f09538941c4cdf9e697ac25968371
sumitprydotintuit <abc@example.com>
2024-10-02 12:48:00 +0530

    Changes for deployment

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,9 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 import cookieParser from "cookie-parser";
 const PORT = process.env.PORT || 5000;
 import { app, server } from "./socket/socket.js";
+import path from "path";
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 
@@ -18,6 +21,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectToMongoDB();
